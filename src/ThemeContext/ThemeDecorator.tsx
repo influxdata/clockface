c655e42885fbc9fd22a4c7cb62fb9a3e0b5c9590
Daniel Campbell <metalwhirlwind@gmail.com>
2018-11-09 10:15:34 -0800

    Define type in decorator

--- src/ThemeContext/ThemeDecorator.tsx
+++ src/ThemeContext/ThemeDecorator.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { ThemeContextProvider } from '../ThemeContext/ThemeContext'
 import { clockfaceTheme } from '../types/themes'
 
-export const themeDecorator = (getStory:any):any => (
+export const themeDecorator = (getStory:() => JSX.Element): JSX.Element => (
   <ThemeContextProvider theme={clockfaceTheme.dark}>
     {getStory()}
   </ThemeContextProvider>
