08a71b1302e7a130a2d345dec3ae18ef527b1205
Daniel Campbell <metalwhirlwind@gmail.com>
2019-03-28 16:43:11 -0700

    migrate grid

---
+++ src/Components/GridLayout/Grid.stories.tsx
@@ -0,0 +1,38 @@
+import * as React from 'react'
+import {storiesOf} from '@storybook/react'
+
+import {Grid} from './Grid'
+import {GridRow} from './GridRow'
+import {GridColumn} from './GridColumn'
+
+import {withKnobs, select} from '@storybook/addon-knobs'
+import {Columns} from '../../Types'
+import {mapEnumKeys} from '../../../.storybook/utils'
+
+const gridStories = storiesOf('Grids', module).addDecorator(withKnobs)
+
+gridStories.add('Grid Component', () => (
+  <Grid>
+    <GridRow>
+      <GridColumn
+        widthXS={Columns[select('widthXS', mapEnumKeys(Columns), 'Five')]}
+        widthSM={Columns[select('widthSM', mapEnumKeys(Columns), 'Five')]}
+        widthMD={Columns[select('widthMD', mapEnumKeys(Columns), 'Five')]}
+        widthLG={Columns[select('widthLG', mapEnumKeys(Columns), 'Five')]}
+        offsetXS={Columns[select('offsetXS', mapEnumKeys(Columns), 'One')]}
+        offsetSM={Columns[select('offsetSM', mapEnumKeys(Columns), 'One')]}
+        offsetMD={Columns[select('offsetMD', mapEnumKeys(Columns), 'One')]}
+        offsetLG={Columns[select('offsetLG', mapEnumKeys(Columns), 'One')]}
+      >
+        <div className="mockComponent stretch">stuff</div>
+      </GridColumn>
+    </GridRow>
+    <GridRow>
+      <GridColumn
+        widthXS={Columns[select('widthXS', mapEnumKeys(Columns), 'Five')]}
+      >
+        <div className="mockComponent stretch">stuff</div>
+      </GridColumn>
+    </GridRow>
+  </Grid>
+))
---
+++ src/Components/GridLayout/GridColumn.tsx
@@ -0,0 +1,73 @@
+// Libraries
+import React, {Component} from 'react'
+import classnames from 'classnames'
+
+// Types
+import {Columns} from '../../Types'
+
+interface DefaultProps {
+  /** Test ID for Integration Tests */
+  testID?: string
+  /** Number of columns spanned when viewport width is less than 750px */
+  widthXS?: Columns
+}
+
+interface PassedProps {
+  /** Number of columns spanned when viewport width is greater than 750px */
+  widthSM?: Columns
+  /** Number of columns spanned when viewport width is greater than 1080px */
+  widthMD?: Columns
+  /** Number of columns spanned when viewport width is greater than 1500px */
+  widthLG?: Columns
+  /** Number of columns shifted when viewport width is less than 750px */
+  offsetXS?: Columns
+  /** Number of columns shifted when viewport width is greater than 750px */
+  offsetSM?: Columns
+  /** Number of columns shifted when viewport width is greater than 1080px */
+  offsetMD?: Columns
+  /** Number of columns shifted when viewport width is greater than 1500px */
+  offsetLG?: Columns
+}
+
+type Props = PassedProps & DefaultProps
+
+export class GridColumn extends Component<Props> {
+  public static defaultProps: DefaultProps = {
+    testID: 'grid--column',
+    widthXS: Columns.Twelve,
+  }
+
+  public render() {
+    const {children, testID} = this.props
+
+    return (
+      <div className={this.className} data-testid={testID}>
+        {children}
+      </div>
+    )
+  }
+
+  private get className(): string {
+    const {
+      widthXS,
+      widthSM,
+      widthMD,
+      widthLG,
+      offsetXS,
+      offsetSM,
+      offsetMD,
+      offsetLG,
+    } = this.props
+
+    return classnames('grid--column', {
+      [`col-xs-${widthXS}`]: widthXS,
+      [`col-sm-${widthSM}`]: widthSM,
+      [`col-md-${widthMD}`]: widthMD,
+      [`col-lg-${widthLG}`]: widthLG,
+      [`col-xs-offset-${offsetXS}`]: offsetXS,
+      [`col-sm-offset-${offsetSM}`]: offsetSM,
+      [`col-md-offset-${offsetMD}`]: offsetMD,
+      [`col-lg-offset-${offsetLG}`]: offsetLG,
+    })
+  }
+}
