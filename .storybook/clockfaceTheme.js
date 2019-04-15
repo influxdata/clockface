879e58b8e7cd75640249e03a41a9e71f8a99ee66
Alex P <thealexpaxton@gmail.com>
2019-04-15 09:21:31 -0700

    Use influx colors in storybook theme

---
+++ .storybook/clockfaceTheme.js
@@ -0,0 +1,37 @@
+import { create } from '@storybook/theming';
+
+export default create({
+  base: 'light',
+
+  colorPrimary: '#22ADF6',
+  colorSecondary: '#9394FF',
+
+  // UI
+  appBg: '#f6f6f8',
+  appContentBg: '#fafafc',
+  appBorderColor: '#eeeff2',
+  appBorderRadius: 4,
+
+  // Typography
+  fontBase: '"Roboto", sans-serif',
+  fontCode: '"RobotoMono", monospace',
+
+  // Text colors
+  textColor: '#545667',
+  textInverseColor: 'rgba(255,255,255,0.9)',
+
+  // Toolbar default and active colors
+  barTextColor: '#a4a8b6',
+  barSelectedColor: '#7A65F2',
+  barBg: '#ffffff',
+
+  // Form colors
+  inputBg: '#ffffff',
+  inputBorder: '#d4d7dd',
+  inputTextColor: '#31313d',
+  inputBorderRadius: 4,
+
+  brandTitle: 'Clockface',
+  brandUrl: 'https://influxdata.github.io/clockface',
+  brandImage: 'https://placehold.it/350x350',
+});
