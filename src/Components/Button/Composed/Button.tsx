fe28dd98a9a995aaaaa7ce2bded5f4aa3ac2eccc
Alex P <thealexpaxton@gmail.com>
2019-04-25 11:44:05 -0700

    Ensure all button props are being passed through

--- src/Components/Button/Composed/Button.tsx
+++ src/Components/Button/Composed/Button.tsx
@@ -65,18 +65,19 @@ export class Button extends Component<Props> {
 
   public render() {
     const {
-      onClick,
-      text,
+      className,
       titleText,
       tabIndex,
-      type,
-      icon,
+      onClick,
       testID,
       status,
-      className,
-      size,
-      color,
       active,
+      color,
+      shape,
+      text,
+      type,
+      icon,
+      size,
     } = this.props
 
     if (!icon && !text) {
@@ -86,15 +87,16 @@ export class Button extends Component<Props> {
     return (
       <ButtonBase
         className={className}
-        status={status}
-        onClick={onClick}
         titleText={titleText || text}
         tabIndex={!!tabIndex ? tabIndex : 0}
-        type={type}
+        onClick={onClick}
         testID={testID}
-        size={size}
-        color={color}
+        status={status}
         active={active}
+        color={color}
+        shape={shape}
+        type={type}
+        size={size}
       >
         {this.iconAndText}
         {this.statusIndicator}
