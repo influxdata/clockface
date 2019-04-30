3f2038a1ca7733861a3886e9d347ba139bb6ae74
Alex P <thealexpaxton@gmail.com>
2019-04-30 15:59:34 -0700

    Extend slide toggle label

--- src/Components/SlideToggle/SlideToggleLabel.tsx
+++ src/Components/SlideToggle/SlideToggleLabel.tsx
@@ -2,22 +2,45 @@
 import React, {Component} from 'react'
 import classnames from 'classnames'
 
+// Types
+import {ComponentSize} from '../../Types'
+
 interface Props {
   /** Text to be displayed as label */
   text: string
+  /** Used to match the state of the associated SlideToggle */
+  active: boolean
+  /** Button size */
+  size: ComponentSize
   /** Class name for custom styles */
   className?: string
+  /** Test ID for Integration Tests */
+  testID: string
 }
 
 export class SlideToggleLabel extends Component<Props> {
+  public static defaultProps = {
+    active: true,
+    size: ComponentSize.Small,
+    testID: 'slide-toggle--label',
+  }
+
   public render() {
-    const {text} = this.props
-    return <label className={this.className}>{text}</label>
+    const {text, testID} = this.props
+    return (
+      <div className={this.className} data-testid={testID}>
+        {text}
+      </div>
+    )
   }
 
   private get className(): string {
-    const {className} = this.props
+    const {className, size, active} = this.props
 
-    return classnames('slide-toggle--label', {[`${className}`]: className})
+    return classnames('slide-toggle--label', {
+      [`${className}`]: className,
+      [`slide-toggle--label-${size}`]: size,
+      'slide-toggle--label__active': active,
+    })
   }
 }
