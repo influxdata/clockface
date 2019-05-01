93a60c00c6c3775dcc1aee2792bc670072480e86
alexpaxton <thealexpaxton@gmail.com>
2019-05-01 10:33:18 -0700

    Bump version 0.0.13 (#119)
    * Bump version
    
    * Build documentation

---
+++ docs/main.e377bd27aec307412fd0.bundle.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[0],{0:function(n,o,p){p(395),p(473),n.exports=p(856)},473:function(n,o,p){"use strict";p.r(o);p(474),p(842),p(853)}},[[0,1,2]]]);
