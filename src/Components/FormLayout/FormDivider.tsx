ac05cbb7b1076390807518abce1ebefbeb5be23e
Alex P <thealexpaxton@gmail.com>
2019-05-01 09:59:12 -0700

    Extend form divider

--- src/Components/FormLayout/FormDivider.tsx
+++ src/Components/FormLayout/FormDivider.tsx
@@ -1,7 +1,15 @@
 // Libraries
-import React, {Component} from 'react'
+import React, {Component, CSSProperties} from 'react'
+import classnames from 'classnames'
+
+// Types
+import {InfluxColors} from '../../Types'
 
 interface Props {
+  /** Optional coloration for horizontal rule in divider */
+  lineColor?: InfluxColors | string
+  /** Class name for custom styles */
+  className?: string
   /** Test ID for Integration Tests */
   testID: string
 }
@@ -14,6 +22,29 @@ export class FormDivider extends Component<Props> {
   public render() {
     const {testID} = this.props
 
-    return <label className="form---divider" data-testid={testID} />
+    return (
+      <div className={this.className} data-testid={testID} style={this.style} />
+    )
+  }
+
+  private get className(): string {
+    const {className, lineColor} = this.props
+
+    return classnames('form--divider', {
+      [`${className}`]: className,
+      'form--divider-line': lineColor,
+    })
+  }
+
+  private get style(): CSSProperties | undefined {
+    const {lineColor} = this.props
+
+    if (lineColor) {
+      return {
+        backgroundColor: lineColor,
+      }
+    }
+
+    return
   }
 }
