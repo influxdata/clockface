61ddd656b2d6d9d178614592f174fc7bc2c9db2a
Alex P <thealexpaxton@gmail.com>
2019-05-16 12:39:57 -0700

    Update styles for markdown rendering

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,36 +1,18 @@
-import React from 'react'
 import {addDecorator, configure, addParameters} from '@storybook/react'
 import clockfaceTheme from './clockfaceTheme'
 import {addReadme} from 'storybook-readme'
-import {configureReadme} from 'storybook-readme'
-// import {withInfo} from '@storybook/addon-info'
-// import {darkTheme, TableComponent} from './StoryLayout'
-
-// addDecorator(
-//   withInfo({
-//     inline: true,
-//     source: false,
-//     styles: darkTheme,
-//     TableComponent,
-//   })
-// )
+import './Story.scss'
 
 addDecorator(addReadme)
-
-// configureReadme({
-//   StoryPreview: ({children}) => (
-//     <div style={{border: '3px solid green'}}>{children}</div>
-//   ),
-//   DocPreview: ({children}) => (
-//     <div style={{background: 'red'}}> {children}</div>
-//   ),
-// })
 
 addParameters({
   options: {
     theme: clockfaceTheme,
     panelPosition: 'right',
   },
+  readme: {
+    codeTheme: 'Dracula',
+  }
 })
 
 const req = require.context('../src/', true, /.stories.tsx$/)
