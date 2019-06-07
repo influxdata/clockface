ed98fc9d4e8297294a6f09b6e74eb6480f38963d
Alex P <thealexpaxton@gmail.com>
2019-06-07 10:43:35 -0700

    Add markdown docs for component spacer

--- src/Components/ComponentSpacer/ComponentSpacer.stories.tsx
+++ src/Components/ComponentSpacer/ComponentSpacer.stories.tsx
@@ -1,5 +1,6 @@
 // Libraries
 import * as React from 'react'
+import marked from 'marked'
 
 // Storybook
 import {storiesOf} from '@storybook/react'
@@ -18,50 +19,63 @@ import {
   ComponentSize,
 } from '../../Types'
 
+// Notes
+const ComponentSpacerReadme = marked(require('./ComponentSpacer.md'))
+
 const componentSpacerStories = storiesOf('Layout|ComponentSpacer', module)
   .addDecorator(withKnobs)
   .addDecorator(jsxDecorator)
 
-componentSpacerStories.add('ComponentSpacer', () => (
-  <ComponentSpacer
-    direction={
-      FlexDirection[select('direction', mapEnumKeys(FlexDirection), 'Row')]
-    }
-    alignItems={
-      AlignItems[select('alignItems', mapEnumKeys(AlignItems), 'Center')]
-    }
-    justifyContent={
-      JustifyContent[
-        select('justifyContent ', mapEnumKeys(JustifyContent), 'FlexStart')
-      ]
-    }
-    margin={
-      ComponentSize[
-        select('margin', {None: '', ...mapEnumKeys(ComponentSize)}, 'None')
-      ]
-    }
-    stretchToFitWidth={boolean('stretchToFitWidth', false)}
-    stretchToFitHeight={boolean('stretchToFitHeight', false)}
-  >
-    <div
-      className="mockComponent box"
-      style={{height: '40px', width: '40px'}}
-    />
-    <div
-      className="mockComponent box"
-      style={{height: '60px', width: '60px'}}
-    />
-    <div
-      className="mockComponent box"
-      style={{height: '80px', width: '80px'}}
-    />
-    <div
-      className="mockComponent box"
-      style={{height: '100px', width: '100px'}}
-    />
-    <div
-      className="mockComponent box"
-      style={{height: '120px', width: '120px'}}
-    />
-  </ComponentSpacer>
-))
+componentSpacerStories.add(
+  'ComponentSpacer',
+  () => (
+    <div className="story--example">
+      <ComponentSpacer
+        direction={
+          FlexDirection[select('direction', mapEnumKeys(FlexDirection), 'Row')]
+        }
+        alignItems={
+          AlignItems[select('alignItems', mapEnumKeys(AlignItems), 'Center')]
+        }
+        justifyContent={
+          JustifyContent[
+            select('justifyContent ', mapEnumKeys(JustifyContent), 'FlexStart')
+          ]
+        }
+        margin={
+          ComponentSize[
+            select('margin', {None: '', ...mapEnumKeys(ComponentSize)}, 'None')
+          ]
+        }
+        stretchToFitWidth={boolean('stretchToFitWidth', false)}
+        stretchToFitHeight={boolean('stretchToFitHeight', false)}
+      >
+        <div
+          className="mockComponent box"
+          style={{height: '40px', width: '40px'}}
+        />
+        <div
+          className="mockComponent box"
+          style={{height: '60px', width: '60px'}}
+        />
+        <div
+          className="mockComponent box"
+          style={{height: '80px', width: '80px'}}
+        />
+        <div
+          className="mockComponent box"
+          style={{height: '100px', width: '100px'}}
+        />
+        <div
+          className="mockComponent box"
+          style={{height: '120px', width: '120px'}}
+        />
+      </ComponentSpacer>
+    </div>
+  ),
+  {
+    readme: {
+      content: ComponentSpacerReadme,
+    },
+  }
+)
