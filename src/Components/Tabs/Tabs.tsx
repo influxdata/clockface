b3c1213ab85d44e92761a66e17fa4f82dee4c16e
alexpaxton <thealexpaxton@gmail.com>
2019-08-14 14:12:56 -0700

    feat: port and refactor Tabs components from InfluxDB (#224)
    * feat: port and refactor tabs components
    
    * docs: write documentation for tabs component family
    
    * refactor: make id arg optional in click handler for tabs
    
    * chore: update changelog

---
+++ src/Components/Tabs/Tabs.tsx
@@ -0,0 +1,84 @@
+// Libraries
+import React, {Component, CSSProperties} from 'react'
+import classnames from 'classnames'
+
+// Components
+import {Tab} from './Tab'
+import {TabContents} from './TabContents'
+import {TabsContainer} from './TabsContainer'
+
+// Types
+import {
+  ComponentSize,
+  StandardProps,
+  Orientation,
+  InfluxColors,
+  Alignment,
+} from '../../Types'
+
+// Styles
+import './Tabs.scss'
+
+interface Props extends StandardProps {
+  /** Layout of tabs */
+  orientation: Orientation
+  /** Controls whether the container renders with padding or not */
+  padding?: ComponentSize
+  /** Background color of tab container */
+  backgroundColor?: InfluxColors | string
+  /** Alignment of tabs within container (perpendicular to orientation) */
+  alignment: Alignment
+}
+
+export class Tabs extends Component<Props> {
+  public static readonly displayName = 'Tabs'
+
+  public static defaultProps = {
+    testID: 'tabs',
+    orientation: Orientation.Horizontal,
+    alignment: Alignment.Left,
+  }
+
+  public static Tab = Tab
+  public static TabContents = TabContents
+  public static Container = TabsContainer
+
+  public render() {
+    const {children, testID, id} = this.props
+
+    return (
+      <nav
+        className={this.className}
+        data-testid={testID}
+        id={id}
+        style={this.style}
+      >
+        {children}
+      </nav>
+    )
+  }
+
+  private get style(): CSSProperties | undefined {
+    const {backgroundColor, style} = this.props
+
+    if (backgroundColor) {
+      return {
+        backgroundColor,
+        ...style,
+      }
+    }
+
+    return style
+  }
+
+  private get className(): string {
+    const {className, orientation, padding, alignment} = this.props
+
+    return classnames('cf-tabs', {
+      [`cf-tabs__align-${alignment}`]: alignment,
+      [`cf-tabs__${orientation}`]: orientation,
+      [`cf-tabs__padding-${padding}`]: padding,
+      [`${className}`]: className,
+    })
+  }
+}
