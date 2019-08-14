947ad0bb8bdc708498ca17d12c350e0a85ca3d53
Alex P <thealexpaxton@gmail.com>
2019-08-14 12:03:47 -0700

    refactor: make id arg optional in click handler for tabs

--- src/Components/Tabs/Tab.tsx
+++ src/Components/Tabs/Tab.tsx
@@ -18,9 +18,9 @@ interface Props extends StandardProps {
   /** Icon to appear left of the text label */
   icon?: JSX.Element
   /** Function to call when tab is clicked, id of tab is passed in */
-  onClick: (id: string) => void
+  onClick: (id?: string) => void
   /** If a function is passed in a dismiss button is rendered in the right of the tab */
-  onDismiss?: (id: string) => void
+  onDismiss?: (id?: string) => void
   /** Background color of tab, text color is determined automatically to optimize contrast */
   backgroundColor: InfluxColors | string
 }
