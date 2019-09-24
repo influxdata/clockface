f6c087c27b1ea1cb53bc07209286769a1e8092e0
Alex P <thealexpaxton@gmail.com>
2019-09-24 09:46:49 -0700

    refactor: convert TextBlock to FunctionComponent and add ref prop

--- src/Components/TextBlock/TextBlock.tsx
+++ src/Components/TextBlock/TextBlock.tsx
@@ -1,89 +1,101 @@
 // Libraries
-import React, {Component, CSSProperties} from 'react'
+import React, {FunctionComponent, CSSProperties, RefObject} from 'react'
 import chroma from 'chroma-js'
 import classnames from 'classnames'
 
 // Types
-import {StandardClassProps, InfluxColors, ComponentSize} from '../../Types'
+import {StandardFunctionProps, InfluxColors, ComponentSize} from '../../Types'
 
 // Styles
 import './TextBlock.scss'
 
-interface Props extends StandardClassProps {
+interface Props extends StandardFunctionProps {
   /** Contents of text block */
   text: string
   /** Sizing of text block, should be the same as the adjacent form elements */
-  size: ComponentSize
+  size?: ComponentSize
   /** Background color of text block */
   backgroundColor?: InfluxColors | string
   /** Color of text, leave blank to have the text color computed for optimal contrast from the background */
   textColor?: InfluxColors | string
   /** Use monospace font instead of default */
-  monospace: boolean
+  monospace?: boolean
+  /** Pass through for ref */
+  ref?: RefObject<HTMLDivElement>
 }
 
-export class TextBlock extends Component<Props> {
-  public static readonly displayName = 'TextBlock'
+export const TextBlock: FunctionComponent<Props> = ({
+  id,
+  ref,
+  text,
+  size = ComponentSize.Small,
+  style,
+  testID = 'text-block',
+  className,
+  monospace = false,
+  textColor,
+  backgroundColor,
+}) => {
+  const textBlockClass = classnames('cf-text-block', {
+    'cf-text-block__monospace': monospace,
+    [`cf-text-block__${size}`]: size,
+    [`${className}`]: className,
+  })
 
-  public static defaultProps = {
-    testID: 'text-block',
-    size: ComponentSize.Small,
-    monospace: false,
-  }
+  const textBlockStyle = generateTextBlockStyle(
+    backgroundColor,
+    textColor,
+    style
+  )
 
-  public render() {
-    const {testID, text, id} = this.props
-
-    return (
-      <div
-        className={this.className}
-        data-testid={testID}
-        id={id}
-        style={this.style}
-      >
-        {text}
-      </div>
-    )
-  }
-
-  private get style(): CSSProperties | undefined {
-    const {backgroundColor, textColor, style} = this.props
-
-    if (!backgroundColor) {
-      return {color: `${textColor}`, ...style}
-    }
-
-    const darkContrast = chroma.contrast(
-      `${backgroundColor}`,
-      InfluxColors.Kevlar
-    )
-    const lightContrast = chroma.contrast(
-      `${backgroundColor}`,
-      InfluxColors.White
-    )
-
-    let color
-
-    if (darkContrast > lightContrast) {
-      color = `${InfluxColors.Kevlar}`
-    } else {
-      color = `${InfluxColors.White}`
-    }
-
-    if (textColor) {
-      color = `${textColor}`
-    }
-
-    return {backgroundColor, color, ...style}
-  }
-
-  private get className(): string {
-    const {className, size, monospace} = this.props
-
-    return classnames('cf-text-block', {
-      'cf-text-block__monospace': monospace,
-      [`cf-text-block__${size}`]: size,
-      [`${className}`]: className,
-    })
-  }
+  return (
+    <div
+      id={id}
+      ref={ref}
+      style={textBlockStyle}
+      data-testid={testID}
+      className={textBlockClass}
+    >
+      {text}
+    </div>
+  )
 }
+
+// Note:
+// This logic should re-use the contrast logic found in /utils
+// once #313 is merged in
+
+const generateTextBlockStyle = (
+  backgroundColor?: InfluxColors | string,
+  textColor?: InfluxColors | string,
+  style?: CSSProperties
+): CSSProperties | undefined => {
+  if (!backgroundColor) {
+    return {color: `${textColor}`, ...style}
+  }
+
+  const darkContrast = chroma.contrast(
+    `${backgroundColor}`,
+    InfluxColors.Kevlar
+  )
+  const lightContrast = chroma.contrast(
+    `${backgroundColor}`,
+    InfluxColors.White
+  )
+
+  let color
+
+  if (darkContrast > lightContrast) {
+    color = `${InfluxColors.Kevlar}`
+  } else {
+    color = `${InfluxColors.White}`
+  }
+
+  if (textColor) {
+    color = `${textColor}`
+  }
+
+  return {backgroundColor, color, ...style}
+}
+
+TextBlock.displayName = 'TextBlock'
