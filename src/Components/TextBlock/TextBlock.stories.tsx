4138ff031a32a5484cdb680b98d8c04dba99dbc2
Alex P <thealexpaxton@gmail.com>
2019-09-25 18:10:40 -0700

    refactor: use forwardRef and ref logger in TextBlock

--- src/Components/TextBlock/TextBlock.stories.tsx
+++ src/Components/TextBlock/TextBlock.stories.tsx
@@ -1,5 +1,5 @@
 // Libraries
-import React, {CSSProperties} from 'react'
+import React, {CSSProperties, RefObject, createRef} from 'react'
 import marked from 'marked'
 
 // Storybook
@@ -36,52 +36,75 @@ const customTextBlockStyles: CSSProperties = {
 
 textBlockStories.add(
   'Example',
-  () => (
-    <div className="story--example">
-      <div style={{margin: '15px'}}>
-        <TextBlock
-          text="No backgroundColor or textColor"
-          size={
-            ComponentSize[select('size', mapEnumKeys(ComponentSize), 'Small')]
-          }
-          monospace={boolean('monospace', false)}
-        />
+  () => {
+    const textBlock1Ref: RefObject<HTMLDivElement> = createRef()
+    const textBlock2Ref: RefObject<HTMLDivElement> = createRef()
+    const textBlock3Ref: RefObject<HTMLDivElement> = createRef()
+    const textBlock4Ref: RefObject<HTMLDivElement> = createRef()
+
+    const logLabelRefs = (): void => {
+      /* eslint-disable */
+      console.log('TextBlock 1', textBlock1Ref.current)
+      console.log('TextBlock 2', textBlock2Ref.current)
+      console.log('TextBlock 3', textBlock3Ref.current)
+      console.log('TextBlock 4', textBlock4Ref.current)
+      /* eslint-enable */
+    }
+
+    return (
+      <div className="story--example">
+        <div style={{margin: '15px'}}>
+          <TextBlock
+            ref={textBlock1Ref}
+            text="No backgroundColor or textColor"
+            size={
+              ComponentSize[select('size', mapEnumKeys(ComponentSize), 'Small')]
+            }
+            monospace={boolean('monospace', false)}
+          />
+        </div>
+        <div style={{margin: '15px'}}>
+          <TextBlock
+            ref={textBlock2Ref}
+            text={text('text', 'I am customizable!')}
+            size={
+              ComponentSize[select('size', mapEnumKeys(ComponentSize), 'Small')]
+            }
+            backgroundColor={color('backgroundColor', '')}
+            monospace={boolean('monospace', false)}
+          />
+        </div>
+        <div style={{margin: '15px'}}>
+          <TextBlock
+            ref={textBlock3Ref}
+            text={text('text', 'I am customizable!')}
+            size={
+              ComponentSize[select('size', mapEnumKeys(ComponentSize), 'Small')]
+            }
+            backgroundColor={color('backgroundColor', '')}
+            textColor={color('textColor', '')}
+            monospace={boolean('monospace', false)}
+          />
+        </div>
+        <div style={{margin: '15px'}}>
+          <TextBlock
+            ref={textBlock4Ref}
+            text="I can be styled"
+            size={
+              ComponentSize[select('size', mapEnumKeys(ComponentSize), 'Small')]
+            }
+            backgroundColor={color('backgroundColor', '')}
+            textColor={color('textColor', '')}
+            monospace={boolean('monospace', false)}
+            style={object('style', customTextBlockStyles)}
+          />
+        </div>
+        <div className="story--test-buttons">
+          <button onClick={logLabelRefs}>Log Refs</button>
+        </div>
       </div>
-      <div style={{margin: '15px'}}>
-        <TextBlock
-          text={text('text', 'I am customizable!')}
-          size={
-            ComponentSize[select('size', mapEnumKeys(ComponentSize), 'Small')]
-          }
-          backgroundColor={color('backgroundColor', '')}
-          monospace={boolean('monospace', false)}
-        />
-      </div>
-      <div style={{margin: '15px'}}>
-        <TextBlock
-          text={text('text', 'I am customizable!')}
-          size={
-            ComponentSize[select('size', mapEnumKeys(ComponentSize), 'Small')]
-          }
-          backgroundColor={color('backgroundColor', '')}
-          textColor={color('textColor', '')}
-          monospace={boolean('monospace', false)}
-        />
-      </div>
-      <div style={{margin: '15px'}}>
-        <TextBlock
-          text="I can be styled"
-          size={
-            ComponentSize[select('size', mapEnumKeys(ComponentSize), 'Small')]
-          }
-          backgroundColor={color('backgroundColor', '')}
-          textColor={color('textColor', '')}
-          monospace={boolean('monospace', false)}
-          style={object('style', customTextBlockStyles)}
-        />
-      </div>
-    </div>
-  ),
+    )
+  },
   {
     readme: {
       content: marked(TextBlockReadme),
--- src/Components/TextBlock/TextBlock.tsx
+++ src/Components/TextBlock/TextBlock.tsx
@@ -1,15 +1,17 @@
 // Libraries
-import React, {FunctionComponent, CSSProperties, RefObject} from 'react'
-import chroma from 'chroma-js'
+import React, {forwardRef} from 'react'
 import classnames from 'classnames'
 
 // Types
 import {StandardFunctionProps, InfluxColors, ComponentSize} from '../../Types'
 
+// Utils
+import {generateTextBlockStyle} from '../../Utils'
+
 // Styles
 import './TextBlock.scss'
 
-interface Props extends StandardFunctionProps {
+export interface TextBlockProps extends StandardFunctionProps {
   /** Contents of text block */
   text: string
   /** Sizing of text block, should be the same as the adjacent form elements */
@@ -20,82 +22,49 @@ interface Props extends StandardFunctionProps {
   textColor?: InfluxColors | string
   /** Use monospace font instead of default */
   monospace?: boolean
-  /** Pass through for ref */
-  ref?: RefObject<HTMLDivElement>
 }
 
-export const TextBlock: FunctionComponent<Props> = ({
-  id,
-  ref,
-  text,
-  size = ComponentSize.Small,
-  style,
-  testID = 'text-block',
-  className,
-  monospace = false,
-  textColor,
-  backgroundColor,
-}) => {
-  const textBlockClass = classnames('cf-text-block', {
-    'cf-text-block__monospace': monospace,
-    [`cf-text-block__${size}`]: size,
-    [`${className}`]: className,
-  })
+export type TextBlockRef = HTMLDivElement
 
-  const textBlockStyle = generateTextBlockStyle(
-    backgroundColor,
-    textColor,
-    style
-  )
+export const TextBlock = forwardRef<TextBlockRef, TextBlockProps>(
+  (
+    {
+      id,
+      text,
+      size = ComponentSize.Small,
+      style,
+      testID = 'text-block',
+      className,
+      monospace = false,
+      textColor,
+      backgroundColor,
+    },
+    ref
+  ) => {
+    const textBlockClass = classnames('cf-text-block', {
+      'cf-text-block__monospace': monospace,
+      [`cf-text-block__${size}`]: size,
+      [`${className}`]: className,
+    })
 
-  return (
-    <div
-      id={id}
-      ref={ref}
-      style={textBlockStyle}
-      data-testid={testID}
-      className={textBlockClass}
-    >
-      {text}
-    </div>
-  )
-}
+    const textBlockStyle = generateTextBlockStyle(
+      backgroundColor,
+      textColor,
+      style
+    )
 
-// Note:
-// This logic should re-use the contrast logic found in /utils
-// once #313 is merged in
-
-const generateTextBlockStyle = (
-  backgroundColor?: InfluxColors | string,
-  textColor?: InfluxColors | string,
-  style?: CSSProperties
-): CSSProperties | undefined => {
-  if (!backgroundColor) {
-    return {color: `${textColor}`, ...style}
+    return (
+      <div
+        id={id}
+        ref={ref}
+        style={textBlockStyle}
+        data-testid={testID}
+        className={textBlockClass}
+      >
+        {text}
+      </div>
+    )
   }
-
-  const darkContrast = chroma.contrast(
-    `${backgroundColor}`,
-    InfluxColors.Kevlar
-  )
-  const lightContrast = chroma.contrast(
-    `${backgroundColor}`,
-    InfluxColors.White
-  )
-
-  let color
-
-  if (darkContrast > lightContrast) {
-    color = `${InfluxColors.Kevlar}`
-  } else {
-    color = `${InfluxColors.White}`
-  }
-
-  if (textColor) {
-    color = `${textColor}`
-  }
-
-  return {backgroundColor, color, ...style}
-}
+)
 
 TextBlock.displayName = 'TextBlock'
