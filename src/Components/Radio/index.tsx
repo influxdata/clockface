a7b4630b23405f02b7df5eccc10829302a45d13b
Alex P <thealexpaxton@gmail.com>
2019-09-26 11:12:06 -0700

    chore: update Radio nomenclature, again!

--- src/Components/Radio/index.tsx
+++ src/Components/Radio/index.tsx
@@ -2,16 +2,16 @@
 import React, {Component} from 'react'
 
 // Components
-import {Radio, RadioProps} from './Radio'
+import {Radio as RadioRoot, RadioProps} from './Radio'
 import {RadioButton} from './RadioButton'
 
-export class RadioFamily extends Component<RadioProps> {
+export class Radio extends Component<RadioProps> {
   public static readonly displayName = 'Radio'
 
-  public static Radio = Radio
+  public static Radio = RadioRoot
   public static Button = RadioButton
 
   render() {
-    return <Radio {...this.props} />
+    return <RadioRoot {...this.props} />
   }
 }
