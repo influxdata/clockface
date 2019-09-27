f520fd6420708b143b372e5110e346a59d4af45b
Alex P <thealexpaxton@gmail.com>
2019-09-27 16:27:49 -0700

    refactor: break out selection indicator into shared component for consistency

---
+++ src/Components/Dropdowns/DropdownItemSelectionIndicator.tsx
@@ -0,0 +1,24 @@
+import React, {FunctionComponent} from 'react'
+
+// Types
+import {DropdownItemType} from '../../Types'
+
+interface DropdownItemSelectionIndicatorProps {
+  type: DropdownItemType
+}
+
+export const DropdownItemSelectionIndicator: FunctionComponent<
+  DropdownItemSelectionIndicatorProps
+> = ({type}) => {
+  switch (type) {
+    case DropdownItemType.Checkbox:
+      return <div className="cf-dropdown-item--checkbox" />
+    case DropdownItemType.Dot:
+      return <div className="cf-dropdown-item--dot" />
+    case DropdownItemType.None:
+    default:
+      return <></>
+  }
+}
+
+DropdownItemSelectionIndicator.displayName = 'DropdownItemSelectionIndicator'
