f998ebf5bbe26b84c3eb16ea77987935f3b1ff41
Alex P <thealexpaxton@gmail.com>
2019-10-14 12:35:16 -0700

    Add all state permutations to story

--- src/Components/SelectableCard/Documentation/SelectableCard.stories.tsx
+++ src/Components/SelectableCard/Documentation/SelectableCard.stories.tsx
@@ -37,17 +37,22 @@ selectableCardStories.add(
     const selectableCard1Ref: RefObject<SelectableCardRef> = createRef()
     const selectableCard2Ref: RefObject<SelectableCardRef> = createRef()
     const selectableCard3Ref: RefObject<SelectableCardRef> = createRef()
+    const selectableCard4Ref: RefObject<SelectableCardRef> = createRef()
 
     const logRefs = (): void => {
       /* eslint-disable */
       console.log('SelectableCard 1', selectableCard1Ref.current)
       console.log('SelectableCard 2', selectableCard2Ref.current)
       console.log('SelectableCard 3', selectableCard3Ref.current)
+      console.log('SelectableCard 4', selectableCard4Ref.current)
       /* eslint-enable */
     }
 
     return (
       <div className="story--example" style={{height: '400px'}}>
+        <div className="story--test-buttons">
+          <button onClick={logRefs}>Log Refs</button>
+        </div>
         <SelectableCard
           ref={selectableCard1Ref}
           style={object('style', exampleStyle)}
@@ -79,7 +84,7 @@ selectableCardStories.add(
         <SelectableCard
           ref={selectableCard2Ref}
           style={object('style', exampleStyle)}
-          id={text('id', 'Titular Title')}
+          id="selected-card"
           icon={
             IconFont[
               select(
@@ -89,9 +94,39 @@ selectableCardStories.add(
               )
             ]
           }
-          label={text('label', 'Titular Title')}
-          selected={boolean('selected', true)}
-          disabled={boolean('disabled', false)}
+          label="Selected"
+          selected={true}
+          disabled={false}
+          fontSize={
+            ComponentSize[select('size', mapEnumKeys(ComponentSize), 'Small')]
+          }
+          color={
+            ComponentColor[
+              select('color', mapEnumKeys(ComponentColor), 'Success')
+            ]
+          }
+          onClick={() => {
+            alert('card clicked!')
+          }}
+        >
+          <div className="mockComponent stretch">Image</div>
+        </SelectableCard>
+        <SelectableCard
+          ref={selectableCard2Ref}
+          style={object('style', exampleStyle)}
+          id="selected-disabled-card"
+          icon={
+            IconFont[
+              select(
+                'icon',
+                {None: 'none', ...mapEnumKeys(IconFont)},
+                'Checkmark'
+              )
+            ]
+          }
+          label="Selected + Disabled"
+          selected={true}
+          disabled={true}
           fontSize={
             ComponentSize[select('size', mapEnumKeys(ComponentSize), 'Small')]
           }
@@ -109,7 +144,7 @@ selectableCardStories.add(
         <SelectableCard
           ref={selectableCard3Ref}
           style={object('style', exampleStyle)}
-          id={text('id', 'Titular Title')}
+          id="default-card"
           icon={
             IconFont[
               select(
@@ -119,9 +154,9 @@ selectableCardStories.add(
               )
             ]
           }
-          label={text('label', 'Titular Title')}
+          label="Default"
           selected={false}
-          disabled={boolean('disabled', false)}
+          disabled={false}
           fontSize={
             ComponentSize[select('size', mapEnumKeys(ComponentSize), 'Small')]
           }
@@ -136,9 +171,36 @@ selectableCardStories.add(
         >
           <div className="mockComponent stretch">Image</div>
         </SelectableCard>
-        <div className="story--test-buttons">
-          <button onClick={logRefs}>Log Refs</button>
-        </div>
+        <SelectableCard
+          ref={selectableCard3Ref}
+          style={object('style', exampleStyle)}
+          id="disabled-card"
+          icon={
+            IconFont[
+              select(
+                'icon',
+                {None: 'none', ...mapEnumKeys(IconFont)},
+                'Checkmark'
+              )
+            ]
+          }
+          label="Disabled"
+          selected={false}
+          disabled={true}
+          fontSize={
+            ComponentSize[select('size', mapEnumKeys(ComponentSize), 'Small')]
+          }
+          color={
+            ComponentColor[
+              select('color', mapEnumKeys(ComponentColor), 'Success')
+            ]
+          }
+          onClick={() => {
+            alert('card clicked!')
+          }}
+        >
+          <div className="mockComponent stretch">Image</div>
+        </SelectableCard>
       </div>
     )
   },
