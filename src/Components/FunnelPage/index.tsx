0c78371c675c88e98f0e394054396e0c2c17bdcc
alexpaxton <thealexpaxton@gmail.com>
2019-11-12 15:56:02 -0500

    Flesh out styles for FunnelPageFooter

--- src/Components/FunnelPage/index.tsx
+++ src/Components/FunnelPage/index.tsx
@@ -4,12 +4,14 @@ import React, {Component} from 'react'
 // Components
 import {FunnelPageRoot, FunnelPageProps} from './Family/FunnelPage'
 import {FunnelPageFooter} from './Family/FunnelPageFooter'
+import {FunnelPageFooterSection} from './Family/FunnelPageFooterSection'
 
 export class FunnelPage extends Component<FunnelPageProps> {
   public static readonly displayName = 'FunnelPage'
 
   public static FunnelPage = FunnelPageRoot
   public static Footer = FunnelPageFooter
+  public static FooterSection = FunnelPageFooterSection
 
   render() {
     return <FunnelPageRoot {...this.props} />
@@ -18,3 +20,4 @@ export class FunnelPage extends Component<FunnelPageProps> {
 
 export {FunnelPageProps, FunnelPageRef} from './Family/FunnelPage'
 export * from './Family/FunnelPageFooter'
+export * from './Family/FunnelPageFooterSection'
