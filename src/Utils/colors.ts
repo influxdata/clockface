3bb1c19d05920f575c9b783766521868389ccac8
alexpaxton <thealexpaxton@gmail.com>
2020-03-19 16:44:44 -0700

    Update contrast tester

--- src/Utils/colors.ts
+++ src/Utils/colors.ts
@@ -1,5 +1,6 @@
 // Libraries
 import {CSSProperties} from 'react'
+import chroma from 'chroma-js'
 
 // Types
 import {Gradients, Gradient, DropdownMenuTheme} from '../Types'
@@ -31,4 +32,21 @@ export const generateInlineCSSGradient = (
   const joinedProperties = [angleText, ...colorsText].join(', ')
 
   return {background: `linear-gradient(${joinedProperties})`}
+}
+
+export const getAverageColorFromLinearGradient = (
+  linearGradient: string
+): string => {
+  const rgbColors = linearGradient.match(
+    /[rR][gG][bB][(]\d+[,]\s\d+[,]\s\d+[)]/g
+  )
+
+  if (rgbColors && rgbColors.length === 2) {
+    const startColor = chroma(rgbColors[0])
+    const stopColor = chroma(rgbColors[1])
+
+    return `${chroma.mix(startColor, stopColor)}`
+  }
+
+  return 'fail'
 }
