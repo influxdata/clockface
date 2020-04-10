9ef2cb677854921eb81e2894b7c0c8cb5c1a0222
alexpaxton <thealexpaxton@gmail.com>
2020-04-10 14:58:27 -0700

    Break resource card meta into its own component

---
+++ src/Components/ResourceList/Card/ResourceCardMeta.tsx
@@ -0,0 +1,72 @@
+// Libraries
+import React, {forwardRef} from 'react'
+import classnames from 'classnames'
+
+// Components
+import {FlexBox, FlexBoxProps, FlexBoxRef} from '../../FlexBox'
+
+// Types
+import {Omit, FlexDirection, AlignItems} from '../../../Types'
+
+// Styles
+import './ResourceCardMeta.scss'
+
+export interface ResourceCardMetaProps
+  extends Omit<
+    FlexBoxProps,
+    'stretchToFitWidth' | 'stretchToFitHeight' | 'margin'
+  > {}
+
+export type ResourceCardMetaRef = FlexBoxRef
+
+export const ResourceCardMeta = forwardRef<
+  ResourceCardMetaRef,
+  ResourceCardMetaProps
+>(
+  (
+    {
+      id,
+      style,
+      testID = 'resource-list--meta',
+      children,
+      className,
+      direction = FlexDirection.Row,
+      alignItems = AlignItems.Center,
+      justifyContent,
+    },
+    ref
+  ) => {
+    const resourceCardMetaClass = classnames('cf-resource-meta', {
+      [`${className}`]: className,
+    })
+
+    let wrappedChildren = children
+
+    if (React.Children.count(children) > 0) {
+      const childArray: JSX.Element[] = []
+
+      React.Children.forEach(children, child => {
+        childArray.push(<div className="cf-resource-meta--item">{child}</div>)
+      })
+
+      wrappedChildren = <>{childArray}</>
+    }
+
+    return (
+      <FlexBox.FlexBox
+        id={id}
+        ref={ref}
+        style={style}
+        testID={testID}
+        direction={direction}
+        className={resourceCardMetaClass}
+        alignItems={alignItems}
+        justifyContent={justifyContent}
+      >
+        {wrappedChildren}
+      </FlexBox.FlexBox>
+    )
+  }
+)
+
+ResourceCardMeta.displayName = 'ResourceCardMeta'
