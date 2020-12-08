098623f401436e737a43c01124b70215285e67a3
alexpaxton <thealexpaxton@gmail.com>
2020-12-08 10:26:13 -0800

    refactor: set theme state on body element
    This ensures portal elements can be receive theming

--- src/Components/ThemeProvider/index.tsx
+++ src/Components/ThemeProvider/index.tsx
@@ -1,5 +1,5 @@
 // Libraries
-import React, {FC, createContext} from 'react'
+import React, {FC, createContext, useEffect} from 'react'
 
 // Types
 import {Theme} from '../../Types'
@@ -18,10 +18,28 @@ export const DEFAULT_THEME = {
 
 export const ThemeContext = createContext<ThemeContext>(DEFAULT_THEME)
 
+const GetThemeClassName = (theme: Theme): string => {
+  return `cf-body__${theme}`
+}
+
 export const ThemeProvider: FC<ThemeProviderProps> = ({
   theme = Theme.Dark,
   children,
 }) => {
+  const handleSetThemeOnBody = (theme: Theme): void => {
+    if (theme === Theme.Dark) {
+      document.body.classList.remove(GetThemeClassName(Theme.Light))
+      document.body.classList.add(GetThemeClassName(Theme.Dark))
+    } else if (theme === Theme.Light) {
+      document.body.classList.remove(GetThemeClassName(Theme.Dark))
+      document.body.classList.add(GetThemeClassName(Theme.Light))
+    }
+  }
+
+  useEffect(() => {
+    handleSetThemeOnBody(theme)
+  }, [theme])
+
   return (
     <ThemeContext.Provider value={{theme}}>{children}</ThemeContext.Provider>
   )
