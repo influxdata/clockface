c34bc4d164e3a000bc27436bf734fc576d720a31
alexpaxton <thealexpaxton@gmail.com>
2020-12-15 14:14:14 -0800

    WIP add escape functionality

--- src/Components/Overlay/Overlay.tsx
+++ src/Components/Overlay/Overlay.tsx
@@ -1,5 +1,5 @@
 // Libraries
-import React, {FunctionComponent, CSSProperties} from 'react'
+import React, {FunctionComponent, CSSProperties, KeyboardEvent} from 'react'
 import {Transition} from 'react-spring/renderprops'
 import classnames from 'classnames'
 import * as easings from 'd3-ease'
@@ -16,6 +16,7 @@ import {StandardFunctionProps, InfluxColors} from '../../Types'
 
 // Styles
 import './Overlay.scss'
+import {useEffect} from '@storybook/addons'
 
 export interface OverlayProps extends StandardFunctionProps {
   /** Controls visibility of the overlay */
@@ -24,6 +25,9 @@ export interface OverlayProps extends StandardFunctionProps {
   renderMaskElement?: (style: CSSProperties) => JSX.Element
   /** Controls the transition timing */
   transitionDuration?: number
+
+  /** Controls custom handling functionality */
+  onEscape?: () => void
 }
 
 export const OverlayRoot: FunctionComponent<OverlayProps> = ({
@@ -33,9 +37,22 @@ export const OverlayRoot: FunctionComponent<OverlayProps> = ({
   children,
   className,
   transitionDuration = 360,
+  onEscape,
   renderMaskElement = (style: CSSProperties) => <OverlayMask style={style} />,
 }) => {
-  const {addElementToPortal} = usePortal()
+  const {
+    addElementToPortal,
+    addEventListenerToPortal,
+    removeEventListenerFromPortal,
+  } = usePortal()
+
+  useEffect((): (() => void) => {
+    addEventListenerToPortal('keydown', handleEscapeKey)
+
+    return (): void => {
+      removeEventListenerFromPortal('keydown', handleEscapeKey)
+    }
+  }, [])
 
   const transitionConfig = {
     duration: transitionDuration,
@@ -46,49 +63,57 @@ export const OverlayRoot: FunctionComponent<OverlayProps> = ({
     [`${className}`]: className,
   })
 
-  const overlay = (
-    <>
-      <Transition
-        items={visible}
-        from={{opacity: 0}}
-        enter={{opacity: 0.7}}
-        leave={{opacity: 0}}
-        config={transitionConfig}
-      >
-        {visible => visible && (props => renderMaskElement(props))}
-      </Transition>
-      <Transition
-        items={visible}
-        from={{opacity: 0, transform: 'translateY(44px)'}}
-        enter={{opacity: 1, transform: 'translateY(0)'}}
-        leave={{opacity: 0, transform: 'translateY(44px)'}}
-        config={transitionConfig}
-      >
-        {visible =>
-          visible &&
-          (props => (
-            <DapperScrollbars
-              className={overlayClass}
-              thumbStartColor={InfluxColors.White}
-              thumbStopColor={InfluxColors.Hydrogen}
-              noScrollX={true}
-              autoHide={false}
-              testID={testID}
-              id={id}
-            >
-              <div
-                className="cf-overlay--children"
-                data-testid={`${testID}--children`}
-                style={props}
+  const handleEscapeKey = (e: KeyboardEvent<Window>): void => {
+    if (e.keyCode === 27 && visible) {
+      onEscape && onEscape()
+    }
+  }
+
+  const overlay = () => {
+    return (
+      <>
+        <Transition
+          items={visible}
+          from={{opacity: 0}}
+          enter={{opacity: 0.7}}
+          leave={{opacity: 0}}
+          config={transitionConfig}
+        >
+          {visible => visible && (props => renderMaskElement(props))}
+        </Transition>
+        <Transition
+          items={visible}
+          from={{opacity: 0, transform: 'translateY(44px)'}}
+          enter={{opacity: 1, transform: 'translateY(0)'}}
+          leave={{opacity: 0, transform: 'translateY(44px)'}}
+          config={transitionConfig}
+        >
+          {visible =>
+            visible &&
+            (props => (
+              <DapperScrollbars
+                className={overlayClass}
+                thumbStartColor={InfluxColors.White}
+                thumbStopColor={InfluxColors.Hydrogen}
+                noScrollX={true}
+                autoHide={false}
+                testID={testID}
+                id={id}
               >
-                {children}
-              </div>
-            </DapperScrollbars>
-          ))
-        }
-      </Transition>
-    </>
-  )
+                <div
+                  className="cf-overlay--children"
+                  data-testid={`${testID}--children`}
+                  style={props}
+                >
+                  {children}
+                </div>
+              </DapperScrollbars>
+            ))
+          }
+        </Transition>
+      </>
+    )
+  }
 
   return addElementToPortal(overlay)
 }
