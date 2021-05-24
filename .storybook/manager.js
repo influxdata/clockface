1dc7a7e367febfb0ab6e37f5845ea879e10cedec
Dong Kyun David Kim <dkim@influxdata.com>
2021-05-23 17:25:08 -0700

    use main.js, preview.js, and manager.js instead of addons and config since they are no longer supported

---
+++ .storybook/manager.js
@@ -0,0 +1,6 @@
+import { addons } from '@storybook/addons';
+import clockfaceTheme from './clockfaceTheme'
+
+addons.setConfig({
+    clockfaceTheme,
+  });
---
+++ .storybook/preview.js
@@ -0,0 +1,16 @@
+import {addDecorator, addParameters} from '@storybook/react'
+import clockfaceTheme from './clockfaceTheme'
+import {addReadme} from 'storybook-readme'
+import './Story.scss'
+
+addDecorator(addReadme)
+
+addParameters({
+    options: {
+      theme: clockfaceTheme,
+      panelPosition: 'right',
+    },
+    readme: {
+      codeTheme: 'Dracula',
+    },
+  });
