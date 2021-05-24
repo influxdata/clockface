4b34435f67bd21450b1360fe64ca73596b6a4a09
Dong Kyun David Kim <dkim@influxdata.com>
2021-05-23 21:37:12 -0700

    add support for viewport addons

--- .storybook/main.js
+++ .storybook/main.js
@@ -2,6 +2,7 @@ module.exports = {
     stories: ['../src/**/*.stories.tsx'],
     addons: ['@storybook/addon-knobs',
     'storybook-readme',
-    '@storybook/addon-a11y'
+    '@storybook/addon-a11y',
+    '@storybook/addon-viewport'
 ],
   };
