77e3d17a6026943c50ee2b093fff9630ae6ee78e
Trys Mudford <trys@clearleft.com>
2021-08-04 16:39:35 +0100

    Heading collage

--- src/Components/Typography/Documentation/Heading.stories.tsx
+++ src/Components/Typography/Documentation/Heading.stories.tsx
@@ -8,20 +8,27 @@ import {withKnobs, text, select, boolean} from '@storybook/addon-knobs'
 import {mapEnumKeys} from '../../../Utils/storybook'
 
 // Components
-import {Heading, HeadingRef} from '../'
+import {Heading, HeadingRef} from '..'
 
 // Types
-import {HeadingElement, Typeface, FontWeight} from '../../../Types'
+import {
+  HeadingElement,
+  Typeface,
+  FontWeight,
+  FlexDirection,
+  ComponentSize,
+} from '../../../Types'
 
 // Notes
 import HeadingReadme from './Heading.md'
+import {FlexBox} from '../../FlexBox'
 
 const headingStories = storiesOf('Components/Heading', module).addDecorator(
   withKnobs
 )
 
 headingStories.add(
-  'Example',
+  'Base',
   () => {
     const headingRef: RefObject<HeadingRef> = createRef()
 
@@ -60,6 +67,41 @@ headingStories.add(
         <div className="story--test-buttons">
           <button onClick={logRef}>Log Ref</button>
         </div>
+      </div>
+    )
+  },
+  {
+    readme: {
+      content: marked(HeadingReadme),
+    },
+  }
+)
+
+headingStories.add(
+  'Collage',
+  () => {
+    return (
+      <div className="story--example">
+        <FlexBox direction={FlexDirection.Column} margin={ComponentSize.Large}>
+          <Heading element={HeadingElement.H2} appearance={HeadingElement.H1}>
+            Heading 1
+          </Heading>
+          <Heading element={HeadingElement.H2} appearance={HeadingElement.H2}>
+            Heading 2
+          </Heading>
+          <Heading element={HeadingElement.H2} appearance={HeadingElement.H3}>
+            Heading 3
+          </Heading>
+          <Heading element={HeadingElement.H2} appearance={HeadingElement.H4}>
+            Heading 4
+          </Heading>
+          <Heading element={HeadingElement.H2} appearance={HeadingElement.H5}>
+            Heading 5
+          </Heading>
+          <Heading element={HeadingElement.H2} appearance={HeadingElement.H6}>
+            Heading 6
+          </Heading>
+        </FlexBox>
       </div>
     )
   },
