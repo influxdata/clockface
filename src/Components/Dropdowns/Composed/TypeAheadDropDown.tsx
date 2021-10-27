94bb7e6bff17c02fef8868f92e362cbc6c1c7b6e
Jill R Pelavin <jpelavin@influxdata.com>
2021-10-27 18:13:34 -0400

    Revert "dealing with test idss"
    This reverts commit cf553b571f6c695328926dec8e12e8fa456f046b.

--- src/Components/Dropdowns/Composed/TypeAheadDropDown.tsx
+++ src/Components/Dropdowns/Composed/TypeAheadDropDown.tsx
@@ -1,6 +1,7 @@
 // Libraries
 import React, {ChangeEvent, FC, useRef, useState} from 'react'
 import classnames from 'classnames'
+
 import {Dropdown} from '../.'
 import {MenuStatus} from '../Dropdown'
 
@@ -24,10 +25,6 @@ interface OwnProps extends StandardFunctionProps {
   name?: string
   selectedOption?: SelectableItem | null
   menuTheme?: DropdownMenuTheme
-  disableAutoFocus?: boolean
-  buttonTestId?: string
-  menuTestID?: string
-  itemTestIdPrefix?: string
 }
 
 export const TypeAheadDropDown: FC<OwnProps> = ({
@@ -41,10 +38,6 @@ export const TypeAheadDropDown: FC<OwnProps> = ({
   selectedOption,
   className,
   menuTheme,
-  disableAutoFocus,
-  buttonTestId,
-  menuTestID,
-  itemTestIdPrefix,
 }) => {
   const [typedValue, setTypedValue] = useState<string>('')
   const [selectIndex, setSelectIndex] = useState(-1)
@@ -57,35 +50,28 @@ export const TypeAheadDropDown: FC<OwnProps> = ({
   const [selectedItem, setSelectedItem] = useState<SelectableItem | null>(
     selectedOption
   )
-  /**
-   *  using a ref to hold an instance variable:  what was last typed,
-   * because without this 'click to select' doesn't work.
-   *  (you click to select, which clicks out of the dropdown, so then the dropdown sets
-   * the text to what was last selected.  this works fine for a class component,
-   * but here it uses the stale state of what was previously selected.
-   * this way, what was selected is saved in the ref.
-   */
+  //let backupValue: React.MutableRefObject<{ name: string }>
   let backupValue = useRef<string>('')
 
   if (!menuTheme) {
     menuTheme = DropdownMenuTheme.Onyx
-  }
-  if (!disableAutoFocus) {
-    disableAutoFocus = false
   }
 
   const itemNames = items.map(item => item.name?.toLowerCase())
 
   const filterVals = (event: ChangeEvent<HTMLInputElement>) => {
     const needle = event?.target?.value
+    console.log('in filtervals....')
     // if there is no value, set the shownValues to everything
     // and set the typedValue to nothing (zero it out)
     // reset the selectIndex too
     if (!needle) {
+      console.log('no needle :(')
       setShownValues(items)
       setTypedValue('')
       setSelectIndex(-1)
     } else {
+      console.log('stuff to filter!')
       const result = items.filter(val => {
         const name = val?.name || ''
         return name.toLowerCase().includes(needle.toLowerCase())
@@ -114,6 +100,7 @@ export const TypeAheadDropDown: FC<OwnProps> = ({
     if (!selectedName) {
       selectedName = ''
     }
+    console.log('arghh thank the lord.....', selectedName)
     setTypedValue(selectedName)
   }
 
@@ -140,7 +127,7 @@ export const TypeAheadDropDown: FC<OwnProps> = ({
 
     if (event.keyCode === 13) {
       // return/enter key
-      // lose focus??, reset the selectIndex to -1, & close the menu:
+      // lose focus, reset the selectIndex to -1, & close the menu:
       //event.target.blur()
 
       if (numItems && selectIndex >= 0 && selectIndex < numItems) {
@@ -167,8 +154,10 @@ export const TypeAheadDropDown: FC<OwnProps> = ({
   }
 
   const doSelection = (item: SelectableItem, closeMenuNow?: boolean) => {
+    console.log('just triggered doSelection; with item:', item)
     setSelectedItem(item)
     const actualName = item.name || ''
+    console.log('fubar2 actual name in doselection???', actualName)
     setTypedValue(actualName)
     backupValue.current = actualName
     setSelectIndex(-1)
@@ -180,31 +169,13 @@ export const TypeAheadDropDown: FC<OwnProps> = ({
   }
 
   const onClickAwayHere = () => {
-    //  reset to the selected value; if the user typed in
-    //  something not allowed it will go back to the last selected value:
+    //  reset:
+    console.log('clicking away')
     setTypedValueToSelectedName(backupValue.current)
   }
 
   const dropdownStatus =
     items.length === 0 ? ComponentStatus.Disabled : ComponentStatus.Default
-
-  const isBlank = (pString: string | undefined) =>
-    // Checks for falsiness or a non-white space character
-    !pString || !/[^\s]+/.test(pString)
-
-  const getValueWithBackup = (val: string | undefined, backup: string) => {
-    if (isBlank(val)) {
-      return backup
-    }
-    return val
-  }
-
-  buttonTestId = getValueWithBackup(buttonTestId, 'type-ahead-dropdown--button')
-  menuTestID = getValueWithBackup(menuTestID, 'type-ahead-dropdown--menu')
-  itemTestIdPrefix = getValueWithBackup(
-    itemTestIdPrefix,
-    'type-ahead-dropdown--item'
-  )
 
   // do rendering:
   const inputComponent = (
@@ -222,27 +193,23 @@ export const TypeAheadDropDown: FC<OwnProps> = ({
   return (
     <Dropdown
       {...props}
-      className={className}
+      className="variable-dropdown--dropdown"
       testID={testID || `typeAhead-dropdown--${name}`}
       onClickAway={onClickAwayHere}
       menuOpen={menuOpen}
-      disableAutoFocus={disableAutoFocus}
+      disableAutoFocus
       button={(active, onClick) => (
         <Dropdown.Button
           active={active}
           onClick={onClick}
-          testID={buttonTestId}
+          testID="variable-dropdown--button"
           status={dropdownStatus}
         >
           {inputComponent}
         </Dropdown.Button>
       )}
       menu={onCollapse => (
-        <Dropdown.Menu
-          testID={menuTestID}
-          onCollapse={onCollapse}
-          theme={menuTheme}
-        >
+        <Dropdown.Menu onCollapse={onCollapse} theme={menuTheme}>
           {shownValues.map((value, index) => {
             // add the 'active' class to highlight when arrowing; like a hover
             const classN = classnames({
@@ -256,7 +223,7 @@ export const TypeAheadDropDown: FC<OwnProps> = ({
                 value={value}
                 onClick={doSelection}
                 selected={value.id === selectedItem?.id}
-                testID={`${itemTestIdPrefix}-${value.id}`}
+                testID="dropdown--item"
                 className={classN}
               >
                 {value.name}
