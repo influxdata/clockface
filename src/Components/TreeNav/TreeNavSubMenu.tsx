6355eef7da0515ebe2b7047b24ceb31d0ebd6453
Beth Legesse <legessebl@gmail.com>
2022-05-18 16:34:11 -0500

    prettier

--- src/Components/TreeNav/TreeNavSubMenu.tsx
+++ src/Components/TreeNav/TreeNavSubMenu.tsx
@@ -21,47 +21,52 @@ export type TreeNavSubMenuRef = HTMLDivElement
 export const TreeNavSubMenu = forwardRef<
   TreeNavSubMenuRef,
   TreeNavSubMenuProps
->(({id, style, testID = 'tree-nav--sub-menu', className, children, helpBar}, ref) => {
-  const navMenuHeaderClass = classnames('cf-tree-nav--sub-menu', {
-    [`${className}`]: className,
-  })
-  let popoverPosition = PopoverPosition.ToTheRightTop
-  if (helpBar) {
-    popoverPosition = PopoverPosition.ToTheRight
-  }
-  const triggerRef = useRef<HTMLButtonElement>(null)
+>(
+  (
+    {id, style, testID = 'tree-nav--sub-menu', className, children, helpBar},
+    ref
+  ) => {
+    const navMenuHeaderClass = classnames('cf-tree-nav--sub-menu', {
+      [`${className}`]: className,
+    })
+    let popoverPosition = PopoverPosition.ToTheRightTop
+    if (helpBar) {
+      popoverPosition = PopoverPosition.ToTheRight
+    }
+    const triggerRef = useRef<HTMLButtonElement>(null)
 
-  return (
-    <>
-      <button
-        type="button"
-        className="cf-tree-nav--sub-menu-trigger"
-        ref={triggerRef}
-        aria-label="Show sub menu"
-      />
-      {triggerRef && (
-        <Popover
-          enableDefaultStyles={true}
-          contents={onHide => <div onClick={onHide}>{children}</div>}
-          hideEvent={PopoverInteraction.Hover}
-          showEvent={PopoverInteraction.Hover}
-          triggerRef={triggerRef}
-          position={popoverPosition}
-          className="cf-popover__nav"
-          distanceFromTrigger={4}
+    return (
+      <>
+        <button
+          type="button"
+          className="cf-tree-nav--sub-menu-trigger"
+          ref={triggerRef}
+          aria-label="Show sub menu"
         />
-      )}
-      <div
-        id={id}
-        ref={ref}
-        style={style}
-        className={navMenuHeaderClass}
-        data-testid={testID}
-      >
-        {children}
-      </div>
-    </>
-  )
-})
+        {triggerRef && (
+          <Popover
+            enableDefaultStyles={true}
+            contents={onHide => <div onClick={onHide}>{children}</div>}
+            hideEvent={PopoverInteraction.Hover}
+            showEvent={PopoverInteraction.Hover}
+            triggerRef={triggerRef}
+            position={popoverPosition}
+            className="cf-popover__nav"
+            distanceFromTrigger={4}
+          />
+        )}
+        <div
+          id={id}
+          ref={ref}
+          style={style}
+          className={navMenuHeaderClass}
+          data-testid={testID}
+        >
+          {children}
+        </div>
+      </>
+    )
+  }
+)
 
 TreeNavSubMenu.displayName = 'TreeNavSubMenu'
